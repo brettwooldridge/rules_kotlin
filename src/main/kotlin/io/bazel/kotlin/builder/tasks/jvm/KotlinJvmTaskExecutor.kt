c73ac02d91ceee6e9bc05693f5312e6ba74af265
Benjamin Lee <ben@ben.cm>
2022-01-22 14:13:26 -0800

    Fix the tests

--- src/main/kotlin/io/bazel/kotlin/builder/tasks/jvm/KotlinJvmTaskExecutor.kt
+++ src/main/kotlin/io/bazel/kotlin/builder/tasks/jvm/KotlinJvmTaskExecutor.kt
@@ -76,14 +76,11 @@ class KotlinJvmTaskExecutor @Inject internal constructor(
                     }
                     .given(outputs.abijar).notEmpty {
                       plugin(plugins.jvmAbiGen) {
-                        // TODO(bencodes) We need to support the single-pass abi gen mode
-                        flag("useLegacyAbiGen", "true")
                         flag("outputDir", directories.abiClasses)
                       }
                       given(outputs.jar).empty {
                         plugin(plugins.skipCodeGen)
                       }
-
                     },
                   printOnFail = false)
               } else {
